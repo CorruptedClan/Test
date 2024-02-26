2d2380676c58abdff02586198c824deafaa31146
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2024-02-26 16:44:20 +0200

    Add files via upload

---
+++ ObbyButBike.lua
@@ -0,0 +1,320 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "GenRebornAlphaTest544^H", HidePremium = false, SaveConfig = false, ConfigFolder = "AlphajajIiI9_$", IntroEnabled = true, IntroText = "MUP8700@"})
+
+local Main = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Misc = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Esc = Window:MakeTab({
+	Name = "ESC",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--functions
+function bike()
+if Game.PlaceId == place1 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["99"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place2 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["51"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place3 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place4 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["52"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place5 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place6 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["50"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place7 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["60"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place8 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["41"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place0 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["40"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == placeEz then
+local targetPart = game.Workspace.WorldMap.Checkpoints["41"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place9 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["100"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place10 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["39"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place11 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place12 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place13 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["71"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place14 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["60"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place15 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place16 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place17 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+elseif Game.PlaceId == place18 then
+local targetPart = game.Workspace.WorldMap.Checkpoints["70"]:FindFirstChild("Pole")
+
+if targetPart then
+    local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+    rootPart.CFrame = CFrame.new(targetPart.Position)
+else
+    warn("Target part not found")
+end
+end
+end
+
+--worlds
+place1 = 14184086618
+place2 = 14483937954
+place3 = 14666799841
+place4 = 14840620690
+place5 = 14928421933
+place6 = 14997446013
+place7 = 15063879640
+place8 = 15208940765
+place0 = 15131057422
+placeEz = 15278398765
+place9 = 15321953290
+place10 = 15388975600
+place11 = 15459974550
+place12 = 15524081759
+place13 = 15585510204
+place14 = 15652691914
+place15 = 15816270940
+place16 = 16227489994
+place17 = 16403332463
+place18 = 16487157604
+
+--start
+Main:AddDropdown({
+	Name = "Select Bike",
+	Default = "select a number",
+	Options = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85},
+	Callback = function(Value)
+		biketype = Value
+	end    
+})
+
+Main:AddButton({
+	Name = "Get Bike",
+	Callback = function()
+    	game:GetService("ReplicatedStorage").RemoteEvents.PublishBike:FireServer("BikeType",biketype)
+        game:GetService("ReplicatedStorage").RemoteEvents.ReportDeath:FireServer()
+  	end    
+})
+
+Main:AddButton({
+	Name = "Finish Obby",
+	Callback = function()
+    	bike()
+  	end    
+})
+
+Main:AddToggle({
+	Name = "Trophy Farm (Safe)",
+	Default = false,
+	Callback = function(Value)
+		getgenv().farmsafe = Value
+while getgenv().farmsafe do
+game:GetService("ReplicatedStorage").RemoteEvents.ReportReset:FireServer()
+wait(60)
+bike()
+wait()
+        end
+	end    
+})
+
+Main:AddToggle({
+	Name = "Trophy Farm (Normal)",
+	Default = false,
+	Callback = function(Value)
+		getgenv().farm = Value
+while getgenv().farm do
+game:GetService("ReplicatedStorage").RemoteEvents.ReportReset:FireServer()
+wait(45)
+bike()
+wait()
+        end
+	end    
+})
+
+Main:AddToggle({
+	Name = "Trophy Farm (Fast)",
+	Default = false,
+	Callback = function(Value)
+		getgenv().farmyes = Value
+while getgenv().farmyes do
+game:GetService("ReplicatedStorage").RemoteEvents.ReportReset:FireServer()
+wait(25)
+bike()
+wait(1)
+        end
+	end    
+})
+
+Misc:AddToggle({
+	Name = "Auto Spin Wheel",
+	Default = false,
+	Callback = function(Value)
+		getgenv().spin = Value
+while getgenv().spin do
+game:GetService("ReplicatedStorage").RemoteEvents.SpinWheel:FireServer()
+wait(1)
+        end
+	end    
+})
+
+--escape
+Esc:AddButton({
+	Name = "Destroy UI",
+	Callback = function()
+      		OrionLib:Destroy()
+  	end    
+})
+
+Esc:AddLabel("Version Info: MUP8700@ V1, Stable")
+
+--initialize
+OrionLib:Init()
