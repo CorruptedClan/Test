dd8ae9a19f45f644f5290720ed95729a8544fcec
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2024-02-27 08:26:57 +0200

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,4 +1,2 @@
-loadstring(game:HttpGet(LoadKeyless))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/Test/main/Loader/LoadSrc.lua"))()
 print("Why u Gay and Exploit??")
-LoadKeyless = "https://raw.githubusercontent.com/CorruptedClan/Test/main/Loader/LoadSrc.lua"
-LoadNormal = "https://raw.githubusercontent.com/CorruptedClan/Test/main/Loader/KeySys.lua"
